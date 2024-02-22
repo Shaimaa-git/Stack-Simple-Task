29374c5a1ec5c077dd9e36a2255c47d68a6afdb9
Shaimaa <148558782+Shaimaa-git@users.noreply.github.com>
2024-02-22 12:01:20 +0400

    Update StackInitialization.java

--- StackInitialization.java
+++ StackInitialization.java
@@ -4,14 +4,12 @@ import java.util.Stack;
 public class SimpleStackExample {
     public static void main(String[] args) {
         // Initialize an empty stack with a capacity of 10
-        Stack<Integer> stack = new Stack<>();
-        //make the stack empty
-        stack.empty(); 
+        Stack<Integer> stack = new Stack<>(); 
         //use for loop to set the size of stack up to 10
-        for(int i=0;i>10;i++) {
-            stack.push(i);
+        for(int i=0; i<10; i++) {
+        stack.push(i);
         }
-        //print the empty stack
+        //print the stack
         System.out.println(stack);
 
         }
